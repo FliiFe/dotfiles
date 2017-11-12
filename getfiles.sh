4b28207ac6ed7e7445370d963bec40f819d8de15
FliiFe <theophile.cailliau@gmail.com>
2017-11-12 13:57:13 +0100

    New background, Polybar config reload script, dunst config

--- getfiles.sh
+++ getfiles.sh
@@ -3,7 +3,7 @@
 
 set lasttask ''
 function done_task
-    test "$argv[1]" -eq 0; and echo -e "\e[1;32m✓\e[0m"; or echo -e '\e[1;31m✗\e[0m' $lasttask
+    test "$status" -eq 0; and echo -e "\e[1;32m✓\e[0m"; or echo -e '\e[1;31m✗\e[0m' $lasttask
 end
 
 function new_task
@@ -22,7 +22,7 @@ read -P (printf 'Current directory is \e[32m'(pwd)'\e[0m;, and'$isgit' a git rep
 if test "$answer" = "delete"
     new_task 'Remove current dotfiles'
     rm -rf dotfiles
-    done_task "$status"
+    done_task
 else
     echo 'Aborting.'
     exit
@@ -34,59 +34,70 @@ cd dotfiles
 ## Vim
 new_task 'vimrc'
 cp ~/.vimrc .
-done_task $status
+done_task
 new_task 'vim config'
 mkdir -p .vim
 cp -r ~/.vim/{init.vim,plug.vim} .vim
-done_task $status
+done_task
 
 ## Tmux
 new_task 'tmux config file'
 cp ~/.tmux.conf .
-done_task $status
+done_task
 
 ## i3
 new_task 'i3 config file'
 mkdir -p .config/i3
 cp ~/.config/i3/config .config/i3/config
-done_task $status
-
-## polybar
-new_task 'polybar config file'
-mkdir -p .config/polybar
-cp ~/.config/polybar/{config,weather} .config/polybar/
-done_task $status
-
-## fish
-new_task 'fish config'
-mkdir -p .config/fish
-cp -r ~/.config/fish/{config.fish,functions} .config/fish/
-done_task $status
-
-## urxvt & rofi
-new_task 'rofi and urxvt config (Xresources)'
-cp ~/.Xresources .
-done_task $status
-
-## xsession
-new_task 'xprofile'
-cp ~/.xprofile .
-ln -s ~/.xprofile .xinitrc
-done_task $status
-
-## fehbg and background
-new_task '~/.fehbg and background image'
-cp ~/.fehbg .
-cp -L ~/.bg .
-done_task $status
+done_task
 
 ## i3lock
 new_task 'lock script'
 mkdir bin
 cp ~/bin/lock bin/lock
-done_task $status
+done_task
+
+## polybar
+new_task 'polybar config file'
+mkdir -p .config/polybar
+cp ~/.config/polybar/{config,weather} .config/polybar/
+cp ~/bin/reload-polybar bin/
+done_task
+
+## fish
+new_task 'fish config'
+mkdir -p .config/fish
+cp -r ~/.config/fish/{config.fish,functions} .config/fish/
+done_task
+
+## urxvt & rofi
+new_task 'rofi and urxvt config (Xresources)'
+cp ~/.Xresources .
+done_task
+
+## xsession
+new_task 'xprofile'
+cp ~/.xprofile .
+ln -s ~/.xprofile .xinitrc
+done_task
+
+## fehbg and background
+new_task '~/.fehbg and background image'
+cp ~/.fehbg .
+cp -L ~/.bg .
+done_task
 
 ## Git config
 new_task 'git config'
 cp ~/.gitconfig .gitconfig
-done_task $status
+done_task
+
+## Jump destinations
+new_task 'jump marks'
+cp -r ~/.marks .marks
+done_task
+
+## Dunst config
+new_task 'dunst config'
+cp -r ~/.config/dunst .config/dunst
+done_task
