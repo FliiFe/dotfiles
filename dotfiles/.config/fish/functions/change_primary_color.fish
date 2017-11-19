7f80159f43baea3dfc25a1f6f03f76f8f86c8f94
FliiFe <theophile.cailliau@gmail.com>
2017-11-19 15:32:47 +0100

    Updated tmux config to update pane border color

--- dotfiles/.config/fish/functions/change_primary_color.fish
+++ dotfiles/.config/fish/functions/change_primary_color.fish
@@ -8,5 +8,8 @@ function change_primary_color
     set -gx fish_color_escape $primary
     set -gx fish_color_end $primary
 
+    tmux set -g pane-active-border-fg $primary
+    tmux refresh-client
+
     clear
 end
