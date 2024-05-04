7374dc4254f71b60e9faf57aab221918297020db
Théophile Cailliau <theophile.cailliau@gmail.com>
2024-05-04 14:36:50 +0200

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
     type -q starship; and starship init fish | source
     type -q pyenv; and pyenv init - | source
-    type -q keychain; and eval (keychain --eval);
+    type -q keychain; and eval (keychain --eval --quiet);
 end
